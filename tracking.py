6550338718f612723976dc9906b998603343c0cd
ViktorStoj <64874653+ViktorStoj@users.noreply.github.com>
2025-06-09 18:22:07 -0400

    Update tracking.py
    Added check for time difference between radar and camera during fusion.

--- tracking.py
+++ tracking.py
@@ -54,8 +54,6 @@ def process_queues(stop_event,
     image_buffer = []
     radar_buffer = []
 
-    radar_total = [['timestamp', 'x', 'x_v', 'y', 'y_v']]
-    img_temp = [['azi', 'ela', 'dist']]
 
     detect_output = [['timestamp_image', 'timestamp_radar', 'azimuth', 'elevation', 'distance_camera', 'distance_radar']]
     detect_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")[:-3]
@@ -145,8 +143,7 @@ def process_queues(stop_event,
             # print("Data Fusion detected_________________________________________")
 
             img_time = image_detections_in_window[-1].timestamp
-            print(image_detections_in_window[-1].timestamp)
-
+            print("Nearest date from camera list :" + str(img_time))
             i = 0
             timestamps = []
             while i < len(radar_detections_in_window):
@@ -160,23 +157,33 @@ def process_queues(stop_event,
             res = cloz_dict[min(cloz_dict.keys())]
             print("Nearest date from radar list : " + str(res))
             combination_index = timestamps.index(res)
+            
+            time_diff =  img_time - radar_detections_in_window[combination_index].timestamp 
+            print("Time difference:" + str(time_diff.total_seconds()))
+            if abs(time_diff.total_seconds()) > 1:
+                print("Time difference is too large, skipping data fusion")
+            else:
+                combined_timestamp = max(
+                    image_detections_in_window[-1].timestamp, radar_detections_in_window[-1].timestamp
+                )
+                combined_detections = (
+                        [img_time] + [radar_detections_in_window[combination_index].timestamp]  + image_detections_in_window[-1].detections[0].get_data() + radar_detections_in_window[ combination_index].detections
+                        
+                )
+                print("data fusion___________________________________________________________")
+                 
+                detect_output.append(combined_detections)
 
-            combined_timestamp = max(
-                image_detections_in_window[-1].timestamp, radar_detections_in_window[-1].timestamp
-            )
-            combined_detections = (
-                    [img_time] + [radar_detections_in_window[ combination_index].timestamp]  + image_detections_in_window[-1].detections[0].get_data() + radar_detections_in_window[ combination_index].detections
-                    
-            )
-            print("data fusion___________________________________________________________")
-            # print(combined_detections)
-            detect_output.append(combined_detections)
+                np.savetxt(detect_output_file_path, detect_output, delimiter=",", fmt='% s')
 
-            np.savetxt(detect_output_file_path, detect_output, delimiter=",", fmt='% s')
-
+            #Offline only
+            # radar_buffer_length = len(radar_detections_in_window)
+            # i = 0
+            # while i< radar_buffer_length:
+            #     radar_buffer.append(radar_detections_in_window[i]) 
+            #     i = i+1
 
             # Add the code from Sina to communicate with server
-
             # import socket
             # import json
             #
@@ -207,17 +214,11 @@ def process_queues(stop_event,
             image_timestamp = image_detections_in_window[-1].timestamp
             image_detections = image_detections_in_window[-1].detections
 
-            # print("Timestamps img,rad==========================")
-            # print(image_detections_in_window)
 
             # tracker.update_tracks(image_detections, image_timestamp, type="image_only")
 
         elif radar_detections_in_window:
-            # print("Timestamps ,rad only____________________________")
-
-            # print(len(radar_detections_in_window))
-            # print(radar_detections_in_window)
-
+            
             radar_timestamp = radar_detections_in_window[-1].timestamp
             radar_detections = radar_detections_in_window[-1].detections
             radar_buffer_length = len(radar_detections_in_window)
@@ -226,7 +227,7 @@ def process_queues(stop_event,
                 radar_buffer.append(radar_detections_in_window[i]) 
                 i = i+1
             # tracker.update_tracks([radar_detections], radar_timestamp, type="radar_only")
-
+             
         else:
             last_batch_time = datetime.now()  # Set the start of the next batch window
 
