57804a3205f4640503d74183bed09c2ce3cc976f
unknown <hliu308@uottawa.ca>
2025-06-20 14:45:21 -0400

    update socket to support multi-level flags

--- Prerequisites/server.py
+++ Prerequisites/server.py
@@ -9,13 +9,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     print("Server listening on", HOST, PORT)
 
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
             data = conn.recv(1024)
             if not data:
-                break
+                print("No data received, closing connection.")
+                continue
+
             try:
                 received = json.loads(data.decode())
                 print("Received:", received)
